c71e148fc02afe42b7d9bb47d273139bbfbf1004
Darren Campbell <darren2802@gmail.com>
2019-12-15 20:21:55 -0700

    Add tests and model for book

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :publication_year}
+    it {should validate_presence_of :pages}
+  end
+  describe 'relationships' do
+    it{should have_many :book_authors}
+    it{should have_many(:authors).through(:book_authors)}
+  end
+end
