281c8fa87db4b36d7d302ec98a26127e1d3033aa
Darren Campbell <darren2802@gmail.com>
2019-12-16 05:36:15 -0700

    Add data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p_coehlo = Author.create(name: 'Paulo Coelho')
+j_austen = Author.create(name: 'Jane Austen')
+p_coehlo.books.create(title: 'The Alchemist', publication_year: '1988', pages: 204)
+p_coehlo.books.create(title: 'The Valkyries', publication_year: '1988', pages: 398)
+j_austen.books.create(title: 'Pride and Prejudice', publication_year: '1813', pages: 433)
+j_austen.books.create(title: 'Sense and Sensibility', publication_year: '1811', pages: 374)
